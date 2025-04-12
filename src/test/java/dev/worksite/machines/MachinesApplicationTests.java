fb72527a3b3726b96deb70ebd95187251fe818cf
Jenny Winters <jfwintersdev@gmail.com>
2025-04-12 15:18:02 +0100

    Initial

---
+++ src/test/java/dev/worksite/machines/MachinesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.worksite.machines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
